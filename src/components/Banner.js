a68b431d9285378df31f6312d15de6baebc9a102
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2024-02-26 22:18:53 +0530

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,73 @@
 import React from 'react';
-
+import avatar from '../assets/avatar.webp';
+import { FaGithub, FaLinkedinIn, FaCode } from 'react-icons/fa'
+import { TypeAnimation } from 'react-type-animation';
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className='section' id='home'>
+      <div className='container mx-auto  '>
+        <div className='flex max-md:flex-col-reverse lg:justify-between items-center'>
+          <div>
+          <div className=''>
+            <h1
+            
+            className='text-[55px] font-bold leading-[0.8] lg:text-[70px] '>
+              ELDHOSE <span>IYPE</span>
+            </h1>
+          </div>
+          <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+            <span className='text-white mr-4'>
+              I am a
+            </span>
+            <TypeAnimation sequence={[
+              'Student',
+              2000,
+              'Developer',
+              2000,
+              'Problem Solver',
+              2000,
+            ]}
+              speed={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+            />
+          </div>
+          <p className='mb-8 max-w-lg lg-mx-0'>
+            Motivated B.Tech student with strong leadership abilities and a genuine passion for web technologies.Actively seeking
+            an entry-level role to leverage my adaptability and contribute to a collaborative team with a passion for building and
+            maintaining reliable systems
+          </p>
+          <div className='flex flex-col 
+          '>
+              <div>
+                <button className='btn btn-lg'> Contact me</button>
+                <a href='#' className='text-gradient btn-link ml-10'>
+                  My Portfolio
+                </a>
+              </div>
+              <div className='flex  text-[20px] gap-x-6 max-w-max mt-5 ml-2'>
+                <a href='#'>
+                  <FaGithub/>
+                </a>
+                <a href='#'>
+                  <FaLinkedinIn/>
+                </a>
+                <a href='#'>
+                  <FaCode/>
+                </a>
+              </div>
+          </div>
+          </div>
+          <div>
+            <motion.img initial={{opacity:0,y:100}} animate={{ opacity: 1,y:0 }} transition={{duration: 1, ease: "easeOut"}}
+              src={avatar} alt='avatar' />
+          </div>
+        </div>
+      </div>
+    </section>);
 };
 
 export default Banner;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import logo from '../assets/logo.webp'
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <header id='header' className='py-5  flex justify-between items-center  ml-10'>
+
+
+    <a href='#'>
+      <img src={logo} alt='logo' className='h-16 ' />
+    </a>
+
+
+
+  </header>);
 };
 
 export default Header;
