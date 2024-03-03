2bb560e7c42e19ac09d033c58102ddbcd6759e3a
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2024-03-03 13:00:21 +0530

    added files

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,37 +2,32 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { FaGithub } from 'react-icons/fa';
 import { LuLink } from "react-icons/lu";
-import img1 from '../assets/portfolio-img1.png';
-import img2 from '../assets/portfolio-img2.png';
-import img3 from '../assets/portfolio-img3.png';
-
-const ProjectCard = ({ imgSrc, projectTitle, projectDescription }) => (
+import {project_data} from './mockdata'
+import p1 from '../assets/projects/p1.webp'
+const ProjectCard = ({ imgSrc, projectTitle, projectDescription,github,live_link }) => (
   <motion.div initial={{opacity:0,y:100}} whileInView={{ opacity: 1,y:0 }} transition={{duration: 1, ease: "easeOut"}}
-   className='  group relative overflow-hidden border-2 border-white/20 rounded-x1 m-5'>
+   className='  group relative overflow-hidden border-2 border-white/20 rounded m-5 w-full h-[50vh]'>
     <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-    <img src={imgSrc} alt='img' className='group-hover:scale-125 transition-all duration-500' />
-    <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-      <span className='text-gradient'>{projectTitle}</span>
-    </div>
-    <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-      <span className='text-3x1 text-white'>{projectDescription}</span>
-    </div>
-    <div className='absolute -bottom-full left-12 group-hover:bottom-8 transition-all duration-700 z-50'>
-      <a href='#'><FaGithub/></a>
-    </div>
-    <div className='absolute -bottom-full left-12 ml-8 group-hover:bottom-8 transition-all duration-700 z-50'>
-      <a href='#'><LuLink/></a>
-    </div>
+    <img src={imgSrc} alt='img' className='group-hover:scale-125 transition-all duration-500 h-full w-full' />
+    
+      <div className='absolute -bottom-full left-12 group-hover:bottom-32 transition-all duration-500 z-50'>
+        <span className='text-gradient'>{projectTitle}</span>
+      </div>
+      <div className='absolute -bottom-full left-12 group-hover:bottom-20 transition-all duration-700 z-50'>
+        <p className='text-sm'>{projectDescription}</p>
+      </div>
+      <div className='absolute -bottom-full left-12 group-hover:bottom-8 transition-all duration-700 z-50'>
+        <a href={github}><FaGithub/></a>
+      </div>
+      <div className='absolute -bottom-full left-12 ml-8 group-hover:bottom-8 transition-all duration-700 z-50'>
+        <a href={live_link}><LuLink/></a>
+      </div>
+      
   </motion.div>
 );
 
 const Projects = () => {
-  const projectData = [
-    { imgSrc: img1, projectTitle: 'React Project', projectDescription: 'Flaourful flare' },
-    { imgSrc: img1, projectTitle: 'React Project', projectDescription: 'Flaourful flare' },
-    { imgSrc: img2, projectTitle: 'React Project', projectDescription: 'Flaourful flare' },
-    { imgSrc: img3, projectTitle: 'React Project', projectDescription: 'Flaourful flare' },
-  ];
+ 
 
   return (
     <div className='section h=[100vh]' id='projects'>
@@ -40,7 +35,7 @@ const Projects = () => {
         <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
           <h2 className='h2 text-accent'>My Projects</h2>
           <div className='grid lg:grid-cols-2 max-md:grid-cols-1 gap-5'>
-            {projectData.map((project, index) => (
+            {project_data.map((project, index) => (
               <ProjectCard key={index} {...project} />
             ))}
           </div>
--- src/components/mockdata.js
+++ src/components/mockdata.js
@@ -1,7 +1,8 @@
 import c1 from '../assets/certificates/certi1.webp';
 import c2 from '../assets/certificates/certi2.webp';
 import c3 from '../assets/certificates/certi3.webp';
-
+import p1 from '../assets/projects/p1.webp';
+import p2 from '../assets/projects/p2.webp';
 export const data = [
     {
       id: 1,
@@ -18,4 +19,21 @@ export const data = [
     
     
   ];
+
+  export const project_data=[
+    {
+      imgSrc: p1,
+       projectTitle: 'Article summarizer', 
+       projectDescription: 'Simplify your reading with article summarizer that transforms lengthy articles into clear and concise summaries' ,
+       github:'https://github.com/Eldhosee/summarizer-',
+       live_link:'https://summarizer2.netlify.app/'
+    },
+    {
+      imgSrc: p2,
+       projectTitle: 'Flavorful Fare', 
+       projectDescription: 'Flavorful Fare is a web application that serves as your ultimate recipe haven.' ,
+       github:'https://github.com/Eldhosee/Flavorful-fare',
+       live_link:'https://flavorful-fare.netlify.app/'
+    }
+  ]
   
