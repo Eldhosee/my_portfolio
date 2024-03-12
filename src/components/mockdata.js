b964a677610bf3ea07aae731be268c657dc4ac79
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2024-03-12 16:07:02 +0530

    new certificate added

--- src/components/mockdata.js
+++ src/components/mockdata.js
@@ -5,43 +5,46 @@ import p1 from '../assets/projects/p1.webp';
 import p2 from '../assets/projects/p2.webp';
 import p3 from '../assets/projects/p3.webp';
 export const data = [
-    {
-      id: 1,
-      img: c1,
-    },
-    {
-      id: 2,
-      img: c2,
-    },
-    {
-        id: 3,
-        img: c3,
-      },
-    
-    
-  ];
+  {
+    id: 1,
+    img: c4,
+  },
+  {
+    id: 2,
+    img: c2,
+  },
+  {
+    id: 3,
+    img: c3,
+  },
+  {
+    id: 4,
+    img: c1
+  }
 
-  export const project_data=[
-    {
-      imgSrc: p1,
-       projectTitle: 'Article summarizer', 
-       projectDescription: 'Simplify your reading with article summarizer that transforms  articles into  concise summaries' ,
-       github:'https://github.com/Eldhosee/summarizer-',
-       live_link:'https://summarizer2.netlify.app/'
-    },
-    {
-      imgSrc: p2,
-       projectTitle: 'Flavorful Fare', 
-       projectDescription: 'Flavorful Fare is a web application that serves as your ultimate recipe haven.' ,
-       github:'https://github.com/Eldhosee/Flavorful-fare',
-       live_link:'https://flavorful-fare.netlify.app/'
-    },
-    {
-      imgSrc: p3,
-       projectTitle: 'Movie Booking ', 
-       projectDescription: 'A robust full-stack project leveraging the Django web framework with a SQLite database backend.' ,
-       github:'https://github.com/Eldhosee/moviebooking',
-       live_link:'https://coderseeker1.pythonanywhere.com/'
-    }
-  ]
-  
+
+];
+
+export const project_data = [
+  {
+    imgSrc: p1,
+    projectTitle: 'Article summarizer',
+    projectDescription: 'Simplify your reading with article summarizer that transforms  articles into  concise summaries',
+    github: 'https://github.com/Eldhosee/summarizer-',
+    live_link: 'https://summarizer2.netlify.app/'
+  },
+  {
+    imgSrc: p2,
+    projectTitle: 'Flavorful Fare',
+    projectDescription: 'Flavorful Fare is a web application that serves as your ultimate recipe haven.',
+    github: 'https://github.com/Eldhosee/Flavorful-fare',
+    live_link: 'https://flavorful-fare.netlify.app/'
+  },
+  {
+    imgSrc: p3,
+    projectTitle: 'Movie Booking ',
+    projectDescription: 'A robust full-stack project leveraging the Django web framework with a SQLite database backend.',
+    github: 'https://github.com/Eldhosee/moviebooking',
+    live_link: 'https://coderseeker1.pythonanywhere.com/'
+  }
+]
